2136e4d843aebd6efc5e63f0f523a068944eaa6a
Arseniy Garelyshev <Ars.garelyshev@yandex.ru>
2019-10-07 12:48:10 +0500

    Added cursor and toString for Block class

--- src/main/kotlin/Map.kt
+++ src/main/kotlin/Map.kt
@@ -5,8 +5,24 @@ enum class Material {
 }
 
 class Block(val material: Material = Material.NONE) {
+    
+    override fun toString(): String {
+        //replace "#" with actual Material -> String map later
+        return "#"
+    }
 }
 
 class Map() {
     val map = Array(256) {Array(256) { Block(Material.DIRT) }}
+}
+
+class Cursor() {
+    //current position of the screen center
+    var posX = 128;
+    var posY = 128;
+    //var posZ = 32; //will be added later
+    
+    //we draw %width blocks to the left of the cursor, and same amount to the rught
+    var width = 30;
+    var height = 12;
 }
