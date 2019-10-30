1a0050e438fc15aea7a89a293c6dc4b418071c42
Arseniy Garelyshev <Ars.garelyshev@yandex.ru>
2019-10-30 20:17:02 +0500

    Dwarfs are generated and placed on the map when game starts

--- src/main/kotlin/game/map/Map.kt
+++ src/main/kotlin/game/map/Map.kt
@@ -1,38 +1,24 @@
 package game.map
 
+import game.actors.Dwarf
 import game.objects.Block
 import game.objects.BlockType
+import next
 import kotlin.random.Random
 
-class Map(val height: Int = 256, val width: Int = 256, depth: Int = 64) {
-    private val random = Random
+class Map(val height: Int = 256, val width: Int = 256, depth: Int = 64, dwarfCount: Int = 1) {
     private val serializer = MapSerializer()
 
-    private val map = Array(height) { Array(width) { Array(depth) { z-> Block(generateMap(z)) }}}
+    private val map: Array<Array<Array<Block>>>
+
+    init {
+        map = Array(height) { x ->  Array(width) { y -> Array(depth) { z-> MapGenerator.generateBlock(x, y, z) }}}
+        MapGenerator.addDwarfs(map, dwarfCount)
+    }
 
     var x = posX
     var y = posY
     var z = posZ
-
-    private fun generateMap(z: Int): BlockType {
-        return when {
-            z > DIRT_HEIGHT + 1 -> BlockType.NONE
-            z == DIRT_HEIGHT + 1 -> {
-                return if (random.nextFloat() < 0.05) BlockType.WOOD else BlockType.NONE
-            }
-            z == DIRT_HEIGHT -> BlockType.DIRT
-            z == 0 -> BlockType.BEDROCK
-            // DIRT_HEIGHT > z > 0
-            else -> {
-                val rnd = random.nextFloat()
-                when {
-                    rnd < 0.02 -> BlockType.GOLD
-                    rnd < 0.05 -> BlockType.IRON
-                    else -> BlockType.STONE
-                }
-            }
-        }
-    }
 
     private fun getVisibleWidthBounds(): Pair<Int, Int> {
         return Pair(x - Cursor.width, x + Cursor.width)
@@ -96,6 +82,85 @@ class Map(val height: Int = 256, val width: Int = 256, depth: Int = 64) {
         //we draw $width$ blocks to the left of the cursor, and same amount to the right
         var width = 30
         var height = 12
+    }
+
+    object MapGenerator {
+        private val random = Random
+
+        fun generateBlock(x: Int, y: Int, z: Int): Block {
+            return when {
+                z > DIRT_HEIGHT + 1 -> Block(BlockType.NONE)
+                z == DIRT_HEIGHT + 1 -> generateSurface()
+                z == DIRT_HEIGHT -> Block(BlockType.DIRT)
+                z == 0 -> Block(BlockType.BEDROCK)
+                else -> generateUnderground(z)
+            }
+        }
+
+        private fun generateSurface(): Block {
+            return if (random.nextFloat() < 0.05) Block(BlockType.WOOD) else Block(BlockType.NONE)
+        }
+
+        private fun generateUnderground(z: Int): Block {
+            val rnd = random.nextFloat()
+            return Block(when {
+                rnd < scaleWithHeight(0.02, z) -> BlockType.GOLD
+                rnd < scaleWithHeight(0.05, z) -> BlockType.IRON
+                else -> BlockType.STONE
+            })
+        }
+
+        /**
+         * At DIRT_HEIGHT base would be halved, at 0 - doubled.
+         */
+        private fun scaleWithHeight(base: Double, z: Int): Double {
+            return base * 2 * (1 - z / (DIRT_HEIGHT * 1.25))
+        }
+
+        /**
+         * Adds dwarfs to the generated map.
+         * Starts at cursor location and goes around in spiral, trying to find empty places to put dwarfs in.
+         *   ┍━━┑
+         *   │┍┑│
+         *  ││+││
+         *  │┕━┙│
+         *  ┕━━━┙
+         *  (that's a spiral, not a paper clip)
+         */
+        fun addDwarfs(map: Array<Array<Array<Block>>>, dwarfCount: Int) {
+            var dwarfsNeeded = dwarfCount
+            var stepSize = 1
+            var stepCounter = 0
+            val MAX_STEPS = 2
+            var direction = Direction.NORTH
+            var x = posX
+            var y = posY
+            //while we need more dwarfs and we are still on the map
+            while (dwarfsNeeded > 0 && map.indices.contains(x) && map[x].indices.contains(y)) {
+                if (map[x][y][posZ].occupant == null && map[x][y][posZ].blockType == BlockType.NONE) {
+                    map[x][y][posZ].occupant = Dwarf(x, y, posZ)
+                    dwarfsNeeded--
+                } else {
+                    if (stepCounter == MAX_STEPS) {
+                        stepSize += 1
+                        stepCounter = 0
+                        direction = direction.next()
+                    }
+
+                    when (direction) {
+                        Direction.NORTH -> y -= 1
+                        Direction.EAST -> x += 1
+                        Direction.SOUTH -> y += 1
+                        Direction.WEST -> x -= 1
+                    }
+                    stepCounter += 1
+                }
+            }
+        }
+
+        private enum class Direction {
+            NORTH, EAST, SOUTH, WEST
+        }
     }
 }
 
--- src/main/kotlin/util.kt
+++ src/main/kotlin/util.kt
@@ -1,3 +1,15 @@
+/**
+ * Returns value bound by 0 and max value
+ */
 fun bound(value: Int, max: Int): Int {
     return value.coerceAtLeast(0).coerceAtMost(max - 1)
+}
+
+/**
+ * Returns next value in enum
+ */
+inline fun <reified T: Enum<T>> T.next(): T {
+    val values = enumValues<T>()
+    val nextOrdinal = (ordinal + 1) % values.size
+    return values[nextOrdinal]
 }
